d5986f6251e56b565b5b002b191366ef228ff98b
Thach Le <levu48@gmail.com>
2021-06-12 22:32:22 -0700

    logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Example project to show howto built CI/CD for a static Website via cdk.
-        </p>
-        <p>
-        Hosted <a href="https://static-site.alst.superluminar.io">here</a>.
-        </p>
-        <p>
-          See <a href="https://superluminar.io/2020/12/04/static-websites-with-aws-cdk/">superluminar.io Blog</a> for further explanation.
-        </p>
+        <img src='/images/PhoBalo_logo6.svg' className="App-logo" alt="logo" />
       </header>
     </div>
   );
